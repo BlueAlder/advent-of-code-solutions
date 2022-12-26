b6fc2e41ad2f958c6bc356ed1ef4ab5da9d0d4cb
Sam Calamos <sam.calamos@gmail.com>
2022-12-26 11:31:56 +1100

    add day 22 p1

--- solutions/day22/solve.py
+++ solutions/day22/solve.py
@@ -3,36 +3,60 @@
 
 import os
 
-open_tiles = set()
-wall_tiles = set()
-
+tiles = set()
 numbers = "0123456789"
 
+def findWrap(ordinate: int , horizontal: bool, wrap_to_start: bool):
+  line = map(lambda p: p[0 if horizontal else 1], filter(lambda p: p[1 if horizontal else 0] == ordinate, tiles))
+  val = min(line) if wrap_to_start else max(line)
+  return (val, ordinate) if horizontal else (ordinate, val)
+
 class Monke:
-  directions = [(1, 0), (0, -1), (-1, 0), (1, 0)]
+  directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
   def __init__(self, position) -> None:
     self.position = position
     self.direction = 0
+  
+  def __str__(self) -> str:
+    return f"{self.position} {self.direction}"
 
   def rotate(self, direction):
     if direction == "R":
-      self.direction = (self.direction + 1) % len(direction) 
+      self.direction = (self.direction + 1) % len(self.directions) 
     elif direction == "L":
-      self.direction = (self.direction - 1) % len(direction)
+      self.direction = (self.direction - 1) % len(self.directions)
     else:
       raise "Bruh"
   
-  def move(self) -> bool:
-    npos = (self.position[0] + self.directions[self.direction][0], self.position[1] + self.directions[self.direction][1])
-    if npos in wall_tiles:
-      return False
-    if npos not in open_tiles:
-      
-
-    
-
+  def startWalking(self, instructions):
+    for instruction in instructions:
+      print(f"Instruction: {instruction}")
+      if isinstance(instruction, int):
+        for i in range(instruction):
+          res = self.move()
+          if not res: break
+      else:
+        self.rotate(instruction)
   
+  def move(self) -> bool:
+    npos = (self.position[0] + self.directions[self.direction][0], self.position[1] + self.directions[self.direction][1], True)
+    if npos in tiles:
+      self.position = npos
+      return True
+    else:
+      npos = (npos[0], npos[1], False)
+      if npos in tiles: return False
 
+    # Wrap
+    row = True if self.direction % 2 == 0 else False
+    ord = self.position[1] if row else self.position[0]
+    wrap_to_start = True if self.direction < 2 else False
+    w = findWrap(ord, row, wrap_to_start)
+    npos = (w[0], w[1], True)
+    if npos in tiles:
+      self.position = npos
+      return True
+    return False      
 
 def parseInput(filename):
   with open(os.path.join(os.path.dirname(__file__), filename)) as f:
@@ -42,9 +66,9 @@ def parseInput(filename):
       if line == "": break
       for x, chr in enumerate(line):
         if chr == ".":
-          open_tiles.add((x + 1, y))
+          tiles.add((x + 1, y, True))
         elif chr == "#":
-          wall_tiles.add((x + 1, y))
+          tiles.add((x + 1, y, False))
       y += 1
     instructionsStr = f.readline()
     instructions = []
@@ -59,21 +83,16 @@ def parseInput(filename):
     if curr_tok: instructions.append(int(curr_tok))
     return instructions
 
-def startWalking(instructions):
-  x = min(map(lambda p: p[0], filter(lambda p: p[1] == 1, open_tiles)))
-  start_position = (x, 1)
-  m = Monke(start_position)
-
-  for instruction in instructions:
-    if isinstance(instruction, int):
-      pass
-    else:
-      m.rotate(instruction)
 
 def solve(filename):
   instructions = parseInput(filename)
-  print(instructions)
-  startWalking(instructions)
+  x = min(map(lambda p: p[0], filter(lambda p: p[1] == 1 and p[2] == True, tiles)))
+  start_position = (x, 1, True)
+  m = Monke(start_position)
+  m.startWalking(instructions)
+  print(m)
+  sol = (m.position[1] * 1000) + (m.position[0] * 4) + m.direction
+  print(sol)
 
 def main():
   solve("input.txt")
