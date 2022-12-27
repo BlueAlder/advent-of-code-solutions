849ced7c5a8c38206d142a75926145f17a5bf6d4
Sam Calamos <sam.calamos@gmail.com>
2022-12-27 22:28:27 +1100

    add day 23 p1

--- solutions/day23/solve.py
+++ solutions/day23/solve.py
@@ -2,6 +2,10 @@
 # Advent of Code Challenge 23
 
 import os
+import functools
+import operator
+from collections import defaultdict
+
 
 elves = []
 position_check = [
@@ -22,8 +26,13 @@ class Elf:
     self.y = y
     self.proposal = None
   
+  def moveToProposal(self) -> None:
+    self.x = self.proposal[0]
+    self.y = self.proposal[1]
+    self.proposal = None
+  
   def __str__(self) -> str:
-    return f"({self.x}, {self.y})"
+    return f"({self.x}, {self.y}), p:{self.proposal}"
 
 def parseInput(filename):
   with open(os.path.join(os.path.dirname(__file__), filename)) as f:
@@ -34,38 +43,67 @@ def parseInput(filename):
 
 def getAdjacentPositions(p):
   # not finsihed
-  directions = [(0, -1), (1, -1), (-1, -1), (0, 1), (1, 1), (-1, 1), (-1, 0), (1, 0) ]    
   adjP = []
-  for dx, dy in directions:
-    adjP.append((p[0] + dx, p[1] + dy))
+  for cardinal in position_check:
+    adjP.append(list(map(lambda x: (p[0] + x[0], p[1] + x[1]), cardinal)))
   return adjP
+
+def getMinMaxCoords():
+  xCoords = list(map(lambda e: e.x, elves))
+  yCoords = list(map(lambda e: e.y, elves))
+  minx = min(xCoords)
+  maxx = max(xCoords)
+  miny = min(yCoords)
+  maxy = max(yCoords)
+  return minx, maxx, miny, maxy
+
+def getEmptyNumberTiles():
+  minx, maxx, miny, maxy = getMinMaxCoords()
+  return (maxx - minx + 1) * (maxy - miny + 1) - len(elves)
 
 def playRound(round_number):
   elfPositions = set()
+  proposals = defaultdict(lambda: 0)
+
   for elf in elves:
     elfPositions.add((elf.x, elf.y))
   
   for elf in elves:
     ap = getAdjacentPositions((elf.x, elf.y))
-    adjElves = elfPositions.intersection(ap)
+    uniqueAp = set(functools.reduce(operator.iconcat, ap, []))
+    adjElves = elfPositions.intersection(uniqueAp)
     if len(adjElves) > 0:
       for i in range(4):
-        if adjElves.isdisjoint(position_check[(i + round_number) % 4])
+        if adjElves.isdisjoint(set(ap[(i + round_number) % 4])):
+          elf.proposal = ap[(i + round_number) % 4][0]
+          proposals[ap[(i + round_number) % 4][0]] += 1
+          break
+  
+  for i, elf in enumerate(elves):
+    if proposals[elf.proposal] == 1:
+      elf.moveToProposal()
+    elf.proposal = None
+    # print(i, elf)
 
 
     
     
 
 
-def solve(filename):
+def solve(filename, rounds):
   parseInput(filename)
-  playRound()
+  # print(getEmptyNumberTiles())
+  for i in range(rounds):
+    playRound(i)
+  return getEmptyNumberTiles()
+  
   # for e in elves:
     # print(e)
   # print(len(elves))
 
 def main():
-  solve("input.txt")
+  p1 = solve("input.txt", 10)
+  print("Part 1:", p1)
 
 if __name__ == "__main__":
   main()
