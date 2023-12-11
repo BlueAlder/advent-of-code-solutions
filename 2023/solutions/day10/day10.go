6b79f1e6174a8c40a97d3c50539f69706114d685
Sam Calamos <sam.calamos@gmail.com>
2023-12-11 12:28:07 +1100

    initial day 10 part 2 partial solution. have a better idea though

--- 2023/solutions/day10/day10.go
+++ 2023/solutions/day10/day10.go
@@ -5,22 +5,25 @@ import (
 	_ "embed"
 	"errors"
 	"fmt"
+	"maps"
+	"slices"
 	"strings"
+
+	util "github.com/BlueAlder/advent-of-code-solutions/utils"
 )
 
 //go:embed input.txt
 var input string
 
 func Solve(part int) int {
-	return part1(input)
-	// if part == 1 {
-	// 	return part1(input)
-	// } else if part == 2 {
-	// 	return part2(input)
-	// } else {
-	// 	util.LogFatal("invalid part number")
-	// 	return -1
-	// }
+	if part == 1 {
+		return part1(input)
+	} else if part == 2 {
+		return part2(input)
+	} else {
+		util.LogFatal("invalid part number")
+		return -1
+	}
 }
 
 type Direction struct {
@@ -60,13 +63,12 @@ func (p Pipe) getExitFromEntry(d Direction) (Direction, error) {
 			return p.exits[idx], nil
 		}
 	}
-	return Direction{0, 0}, errors.New("Invalid Entry")
+	return Direction{0, 0}, errors.New("invalid Entry")
 }
 
 func part1(inputData string) int {
 
 	lines := strings.Split(inputData, "\n")
-
 	// Find S
 	var s Point
 	for y, line := range lines {
@@ -76,7 +78,6 @@ func part1(inputData string) int {
 		}
 
 	}
-	fmt.Println(s)
 	curr := s
 	var direction Direction
 	// Find start
@@ -117,5 +118,93 @@ func part1(inputData string) int {
 }
 
 func part2(inputData string) int {
-	return 0
+	lines := strings.Split(inputData, "\n")
+	// Find S
+	var s Point
+	for y, line := range lines {
+		if x := strings.Index(line, "S"); x != -1 {
+			s = Point{x, y}
+			break
+		}
+
+	}
+	curr := s
+	var direction Direction
+	// Find start
+	for _, dir := range directions {
+		if !isInBounds(lines, Point{curr.x + dir.dx, curr.y + dir.dy}) {
+			continue
+		}
+
+		sym := string(lines[curr.y+dir.dy][curr.x+dir.dx])
+		pipe, exists := pipes[sym]
+		if !exists {
+			continue
+		}
+		if _, err := pipe.getExitFromEntry(dir); err == nil {
+			direction = dir
+			break
+		}
+	}
+
+	// Walk the path and flood fill everything on the right of the
+	//  facing direction
+	area := make(map[Point]struct{})
+	for {
+		curr.x += direction.dx
+		curr.y += direction.dy
+		if curr == s {
+			break
+		}
+
+		// get element to the right of where we are facing
+		rightDirIdx := (((slices.Index(directions, direction) + 1) % len(directions)) + len(directions)) % len(directions)
+		rightDir := directions[rightDirIdx]
+		rightNode := Point{curr.x + rightDir.dx, curr.y + rightDir.dy}
+		if isInBounds(lines, rightNode) && string(lines[rightNode.y][rightNode.x]) == "." {
+			maps.Copy(area, floodFill(rightNode, lines))
+		}
+
+		sym := string(lines[curr.y][curr.x])
+
+		pipe, exists := pipes[sym]
+		if !exists {
+			panic("invalid pipe direction")
+		}
+		var err error
+		direction, err = pipe.getExitFromEntry(direction)
+		if err != nil {
+			panic("invalid direction")
+		}
+	}
+
+	for k := range area {
+		fmt.Println(k)
+	}
+	return len(area)
+}
+
+func isInBounds(lines []string, p Point) bool {
+	xInBound := p.x >= 0 && p.x < len(lines[0])
+	yInBound := p.y >= 0 && p.y < len(lines)
+	return xInBound && yInBound
+}
+
+func floodFill(start Point, lines []string) map[Point]struct{} {
+	queue := []Point{start}
+	floods := make(map[Point]struct{})
+	for len(queue) > 0 {
+		n := queue[0]
+		queue = queue[1:]
+		_, seen := floods[n]
+		if isInBounds(lines, n) && string(lines[n.y][n.x]) == "." &&
+			!seen {
+			floods[n] = struct{}{}
+			queue = append(queue, Point{n.x + 1, n.y})
+			queue = append(queue, Point{n.x, n.y + 1})
+			queue = append(queue, Point{n.x - 1, n.y})
+			queue = append(queue, Point{n.x, n.y - 1})
+		}
+	}
+	return floods
 }
--- 2023/solutions/day10/day10_test.go
+++ 2023/solutions/day10/day10_test.go
@@ -30,9 +30,57 @@ func TestPart1b(t *testing.T) {
 	}
 }
 
-func TestPart2(t *testing.T) {
-	answer := part2(exampleInputA)
-	solution := -1
+const exampleInputC = `...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........`
+
+func TestPart2C(t *testing.T) {
+	answer := part2(exampleInputC)
+	solution := 4
+	if answer != solution {
+		t.Fatalf("Example input failed. Got: %d, Want: %d", answer, solution)
+	}
+}
+
+const exampleInputD = `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...`
+
+func TestPart2D(t *testing.T) {
+	answer := part2(exampleInputD)
+	solution := 8
+	if answer != solution {
+		t.Fatalf("Example input failed. Got: %d, Want: %d", answer, solution)
+	}
+}
+
+const exampleInputE = `FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L`
+
+func TestPart2E(t *testing.T) {
+	answer := part2(exampleInputE)
+	solution := 10
 	if answer != solution {
 		t.Fatalf("Example input failed. Got: %d, Want: %d", answer, solution)
 	}
